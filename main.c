e45280fd9991ac5bc5cbcde9c5a955bb3ca89293
shotgunshine <mc517053@gmail.com>
2025-12-07 15:52:34 +0100

    refactoring

--- main.c
+++ main.c
@@ -19,10 +19,12 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 #include <time.h>
 #include <gtk/gtk.h>
 #include <gtk4-layer-shell/gtk4-layer-shell.h>
-#include <glib-2.0/glib-unix.h>
 
 const char *WDAYS[] = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 const char *MONTHS[] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+static GtkWidget *window = NULL;
+static GtkWidget *calendar = NULL;
 
 static gboolean no_layer_shell = FALSE;
 static gboolean start_hidden = FALSE;
@@ -41,7 +43,7 @@ static gboolean update_date(gpointer user_data) {
 
 static gboolean show_today(GtkWidget *widget) {
 	GDateTime *now = g_date_time_new_now_local();
-	gtk_calendar_select_day(GTK_CALENDAR(widget), now);
+	gtk_calendar_set_date(GTK_CALENDAR(widget), now);
 	return G_SOURCE_CONTINUE;
 }
 
@@ -51,40 +53,42 @@ static gboolean toggle_visible(GtkWidget *widget) {
 }
 
 static void activate(GtkApplication *app, gpointer user_data) {
-	GtkWidget *window = gtk_application_window_new(app);
+	if (window == NULL) {
+		window = gtk_application_window_new(app);
 
-	if (!no_layer_shell) {
-		gtk_layer_init_for_window(GTK_WINDOW(window));
-		gtk_layer_set_layer(GTK_WINDOW(window), default_layer);
-		for (int i = 0; i < GTK_LAYER_SHELL_EDGE_ENTRY_NUMBER; i++)
-			gtk_layer_set_anchor(GTK_WINDOW(window), i, default_anchors[i]);
-	}
+		if (!no_layer_shell) {
+			gtk_layer_init_for_window(GTK_WINDOW(window));
+			gtk_layer_set_layer(GTK_WINDOW(window), default_layer);
+			for (int i = 0; i < GTK_LAYER_SHELL_EDGE_ENTRY_NUMBER; i++)
+				gtk_layer_set_anchor(GTK_WINDOW(window), i, default_anchors[i]);
+		}
 
-	GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-	gtk_widget_add_css_class(window, "frame");
-	gtk_widget_add_css_class(window, "view");
-	gtk_widget_set_margin_top(box, 5);
-	gtk_widget_set_margin_bottom(box, 5);
-	gtk_widget_set_margin_start(box, 5);
-	gtk_widget_set_margin_end(box, 5);
-	gtk_window_set_child(GTK_WINDOW(window), box);
+		GtkWidget *box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
+		gtk_widget_add_css_class(window, "frame");
+		gtk_widget_set_margin_top(box, 5);
+		gtk_widget_set_margin_bottom(box, 5);
+		gtk_widget_set_margin_start(box, 5);
+		gtk_widget_set_margin_end(box, 5);
+		gtk_window_set_child(GTK_WINDOW(window), box);
 
-	GtkWidget *date = gtk_label_new(NULL);
-	PangoAttrList *attrlist = pango_attr_list_new();
-	pango_attr_list_insert(attrlist, pango_attr_font_features_new("tnum=1"));
-	pango_attr_list_insert(attrlist, pango_attr_size_new(15 * PANGO_SCALE));
-	gtk_label_set_attributes(GTK_LABEL(date), attrlist);
-	gtk_box_append(GTK_BOX(box), date);
-	g_timeout_add(100, update_date, date);
+		GtkWidget *date = gtk_label_new(NULL);
+		PangoAttrList *attrlist = pango_attr_list_new();
+		pango_attr_list_insert(attrlist, pango_attr_font_features_new("tnum=1"));
+		pango_attr_list_insert(attrlist, pango_attr_size_new(15 * PANGO_SCALE));
+		gtk_label_set_attributes(GTK_LABEL(date), attrlist);
+		gtk_box_append(GTK_BOX(box), date);
+		g_timeout_add(100, update_date, date);
 
-	GtkWidget *calendar = gtk_calendar_new();
-	gtk_box_append(GTK_BOX(box), calendar);
+		calendar = gtk_calendar_new();
+		gtk_box_append(GTK_BOX(box), calendar);
 
-	gtk_window_present(GTK_WINDOW(window));
-	if (!no_layer_shell) {
-		if (start_hidden) gtk_widget_set_visible(window, FALSE);
-		g_unix_signal_add(SIGUSR1, G_SOURCE_FUNC(toggle_visible), window);
-		g_unix_signal_add(SIGUSR1, G_SOURCE_FUNC(show_today), calendar);
+		gtk_window_present(GTK_WINDOW(window));
+	} else if (no_layer_shell) {
+		show_today(calendar);
+		gtk_window_present(GTK_WINDOW(window));
+	} else {
+		show_today(calendar);
+		toggle_visible(window);
 	}
 }
 
@@ -110,7 +114,6 @@ gboolean anchor_option_callback(const gchar *_option_name, const gchar *value, v
 static const GOptionEntry options[] = {
 	{"anchor", 'a', G_OPTION_FLAG_OPTIONAL_ARG, G_OPTION_ARG_CALLBACK, (void*)&anchor_option_callback, "A sequence of 'l', 'r', 't' and 'b' to anchor to those edges", NULL},
 	{"no-layer-shell", 'n', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &no_layer_shell, "Disable gtk4-layer-shell, create a normal shell surface instead", NULL},
-	{"start-hidden", 'h', G_OPTION_FLAG_NONE, G_OPTION_ARG_NONE, &start_hidden, "Set window not visible at launch (toggle visibility with SIGUSR1)", NULL},
 	G_OPTION_ENTRY_NULL
 };
 
@@ -120,7 +123,7 @@ int main(int argc, char **argv)
 	GOptionContext *context = g_option_context_new("");
 	g_option_context_add_main_entries(context, options, NULL);
 	g_option_context_parse(context, &argc, &argv, &error);
-	GtkApplication *app = gtk_application_new("org.wsimplecal", G_APPLICATION_NON_UNIQUE);
+	GtkApplication *app = gtk_application_new("org.wsimplecal", G_APPLICATION_DEFAULT_FLAGS);
 	g_signal_connect(app, "activate", G_CALLBACK(activate), NULL);
 	int status = g_application_run(G_APPLICATION(app), argc, argv);
 	g_object_unref(app);
